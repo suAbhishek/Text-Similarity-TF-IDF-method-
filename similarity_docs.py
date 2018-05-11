77e2793d0cbd6d3ac3651cb6da81d857c4735b2e
Abhishek R. Kulkarni <ravikumarabhishek10@gmail.com>
2018-05-11 20:23:34 +0530

    Update similarity_docs.py

--- similarity_docs.py
+++ similarity_docs.py
@@ -84,18 +84,6 @@ def get_similarity(doc1,doc2):
     return (1-nltk.cluster.cosine_distance(vec1,vec2))*100
 
 
-if __name__ == '__main__':
-    f1 = open("article1","r")
-    f2 = open("article2","r")
-    text1 = f1.read()
-    text2 = f2.read()
-    print(get_similarity(text1,text2))
-    f1.close()
-    f2.close()
-
-
-
-
 
 
 
